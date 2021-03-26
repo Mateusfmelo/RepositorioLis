99fad12ae1a429af075f5b1e00f07b9c0d9ac481
Mateusfmelo <mateusmelo51@gmail.com>
2021-03-26 14:37:19 -0300

    att

--- Semana05_Q1.js
+++ Semana05_Q1.js
@@ -1,16 +1,15 @@
-let numLista = window.prompt("Digite a quantindade de lista: ")//Pedindo a quantidade de lista que será criada.
-let texto = window.prompt("Digite um nome: ")//Pedindo que irá estar dentro da lista.
+var numLista = window.prompt("Digite a quantindade de lista: ")//Pedindo a quantidade de lista que será criada.
+var texto = window.prompt("Digite um nome: ")//Pedindo que irá estar dentro da lista.
 
-let qtd = 0; //Criando o incrementador.
+var qtd = 0; //Criando o incrementador.
 function myFunction() {
     qtd++ //Sempre que a função for chamada, irá incrementar auqi.
 
-    let criarElemento = document.createElement("LI"); //Fazendo a criacao da tag "<li> </li>" 
-    let textoLista = document.createTextNode(`${texto} ${qtd}º`);//Adicionando o texto digitado pelo o usuario e a numeracao da quantidade da lista ao lado do texto.
+    var criarElemento = document.createElement("LI"); //Fazendo a criacao da tag "<li> </li>" 
+    var textoLista = document.createTextNode(`${texto} ${qtd}`);//Adicionando o texto digitado pelo o usuario e a numeracao da quantidade da lista ao lado do texto.
     criarElemento.appendChild(textoLista);
     document.getElementById("myList").appendChild(criarElemento);//Informando o local que irá ficar a lista.
 };
-
-for (let i = 0; i < numLista; i++){
+for (var i = 0; i < numLista; i++){
     myFunction();
 };
