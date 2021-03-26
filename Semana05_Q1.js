708ff52bc363dcee0d128f910d2628c37f5c4b2e
Mateusfmelo <mateusmelo51@gmail.com>
2021-03-26 16:09:29 -0300

    Atualização

--- Semana05_Q1.js
+++ Semana05_Q1.js
@@ -1,15 +1,13 @@
-var numLista = window.prompt("Digite a quantindade de lista: ")//Pedindo a quantidade de lista que será criada.
-var texto = window.prompt("Digite um nome: ")//Pedindo que irá estar dentro da lista.
+function creatList(quantidadeLi, texto) {
+  console.log("criando lista");
 
-var qtd = 0; //Criando o incrementador.
-function myFunction() {
-    qtd++ //Sempre que a função for chamada, irá incrementar auqi.
+  for (var i = 0; i < quantidadeLi; i++) {
+    var hall = document.createElement("li");
+    var hallzin = document.createTextNode(texto + " " + i);
+    hall.appendChild(hallzin);
 
-    var criarElemento = document.createElement("LI"); //Fazendo a criacao da tag "<li> </li>" 
-    var textoLista = document.createTextNode(`${texto} ${qtd}`);//Adicionando o texto digitado pelo o usuario e a numeracao da quantidade da lista ao lado do texto.
-    criarElemento.appendChild(textoLista);
-    document.getElementById("myList").appendChild(criarElemento);//Informando o local que irá ficar a lista.
-};
-for (var i = 0; i < numLista; i++){
-    myFunction();
-};
+    document.getElementById("myList").appendChild(hall);
+  }
+}
+
+creatList(101, "Item");
